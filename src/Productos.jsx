74e41e560253398ab96cd8856169c1b825a85146
FranciscoTapiaF <frtapiaf@gmail.com>
2025-10-31 18:46:11 -0300

    Agregadas páginas de Contacto y Productos

---
+++ src/Productos.jsx
@@ -0,0 +1,12 @@
+export default function Productos() {
+  return (
+    <div>
+      <h1>Productos</h1>
+      <ul>
+        <li>Producto A</li>
+        <li>Producto B</li>
+        <li>Producto C</li>
+      </ul>
+    </div>
+  );
+}
